d3a60c93711d281710b25574a64e60c548a391ed
Alessandro Gatti <a.gatti@frob.it>
2022-08-01 11:00:28 +0200

    Parse the embedded JSON schema just once.

--- src/main/kotlin/it/frob/kaitaiplugin/KaitaiSchemaInspection.kt
+++ src/main/kotlin/it/frob/kaitaiplugin/KaitaiSchemaInspection.kt
@@ -9,16 +9,38 @@ package it.frob.kaitaiplugin
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.util.ModificationTracker
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
 import com.intellij.util.ResourceUtil
 import com.jetbrains.jsonSchema.extension.JsonLikePsiWalker
 import com.jetbrains.jsonSchema.ide.JsonSchemaService
 import com.jetbrains.jsonSchema.impl.JsonComplianceCheckerOptions
 import com.jetbrains.jsonSchema.impl.JsonSchemaComplianceChecker
+import com.jetbrains.jsonSchema.impl.JsonSchemaObject
 import org.jetbrains.yaml.psi.YAMLFile
 import org.jetbrains.yaml.psi.YamlPsiElementVisitor
+
+private val CACHED_KSY_SCHEMA = CachedValuesManager.getManager(ProjectManager.getInstance().defaultProject)
+    .createCachedValue(object : CachedValueProvider<JsonSchemaObject> {
+        override fun compute(): CachedValueProvider.Result<JsonSchemaObject>? {
+            val virtualFile = VfsUtil.findFileByURL(
+                ResourceUtil.getResource(
+                    javaClass.classLoader,
+                    "schema",
+                    "ksy.schema.json"
+                )
+            ) ?: return null
+            val schema = JsonSchemaService.Impl.get(ProjectManager.getInstance().defaultProject)
+                .getSchemaObjectForSchemaFile(virtualFile) ?: return null
+
+            return CachedValueProvider.Result.create(schema, ModificationTracker.NEVER_CHANGED)
+        }
+    })
 
 class KaitaiSchemaInspection : LocalInspectionTool() {
     override fun buildVisitor(
@@ -26,29 +48,36 @@ class KaitaiSchemaInspection : LocalInspectionTool() {
         isOnTheFly: Boolean,
         session: LocalInspectionToolSession
     ): PsiElementVisitor {
+        if (!holder.file.name.endsWith("ksy")) {
+            return PsiElementVisitor.EMPTY_VISITOR
+        }
         val documents = (holder.file as YAMLFile).documents
         if (documents.size != 1) {
             return PsiElementVisitor.EMPTY_VISITOR
         }
 
-        val virtualFile =
-            VfsUtil.findFileByURL(ResourceUtil.getResource(javaClass.classLoader, "schema", "ksy.schema.json"))
-                ?: return PsiElementVisitor.EMPTY_VISITOR
-        val schema = JsonSchemaService.Impl.get(holder.file.project).getSchemaObjectForSchemaFile(virtualFile)
-            ?: return PsiElementVisitor.EMPTY_VISITOR
-
-        return object : YamlPsiElementVisitor() {
-            override fun visitElement(element: PsiElement) {
-                val walker = JsonLikePsiWalker.getWalker(element, schema) ?: return
-                if (element != documents.first().topLevelValue) return
-                JsonSchemaComplianceChecker(
-                    schema,
-                    holder,
-                    walker,
-                    session,
-                    JsonComplianceCheckerOptions(false)
-                ).annotate(element)
+        CACHED_KSY_SCHEMA.value?.let { schemaObject ->
+            return object : YamlPsiElementVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    val walker = JsonLikePsiWalker.getWalker(element, schemaObject) ?: return
+                    if (element != documents.first().topLevelValue) return
+                    JsonSchemaComplianceChecker(
+                        schemaObject,
+                        holder,
+                        walker,
+                        session,
+                        JsonComplianceCheckerOptions(false)
+                    ).annotate(element)
+                }
             }
+        }?.run {
+            return PsiElementVisitor.EMPTY_VISITOR
         }
+
+        // Needed only to silence the following compiler error:
+        //
+        // A 'return' expression required in a function with a block body ('{...}'). If you got this error after the
+        // compiler update, then it's most likely due to a fix of a bug introduced in 1.3.0 (see KT-28061 for details)
+        return PsiElementVisitor.EMPTY_VISITOR
     }
 }
